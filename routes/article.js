f6d48285416385d4ec8df64b3c5233d5725d19a3
zhufengnodejs <zhufengnodejs@126.com>
2017-07-02 15:11:52 +0800

    增加文章

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router();
 router.get('/add',function(req,res){
-  res.send('增加文章');
+  res.render('article/add',{title:'增加文章'});
 });
 router.get('/delete/:id',function(req,res){
   res.send('删除文章');
