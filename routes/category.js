4d6d12e91333513df7a83fc97532b86add498cfc
zhufengnodejs <zhufengnodejs@126.com>
2017-07-02 12:43:01 +0800

    添加分类

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,9 @@ let express = require('express');
 let {Category} = require('../model');
 let router = express.Router();
 router.get('/list',function(req,res){
-  res.render('category/list',{title:'分类管理'});
+  Category.find({},function(err,categories){
+    res.render('category/list',{title:'分类管理',categories});
+  });
 });
 router.get('/add',function(req,res){
   res.render('category/add',{title:'添加分类'});
@@ -14,7 +16,7 @@ router.post('/add',function(req,res){
       res.back(err);
     }else{
       if(oldCategory){
-        res.back(err);
+        res.back('分类名称已经存在，请换个名称吧');
       }else{
         Category.create(category,function(err,doc){
           if(err){
