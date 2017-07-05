64820fd09891de5c4245c908862d677190fd9d30
zhufengnodejs <zhufengnodejs@126.com>
2017-07-05 19:48:11 +0800

    回显原有的值

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ let router = express.Router();
 router.get('/',function(req,res){
   //populate一个字段表示把一个字段从ID转成对象
   //author外键是一个字段串，
-  Article.find({}).populate('author').exec(function(err,articles){
-    res.render('index',{title:'首页',articles});
+  let {keyword} = req.query;
+  let query = {};
+  if(keyword){
+    query.title = new RegExp(keyword);
+  }
+  Article.find(query).populate('author').exec(function(err,articles){
+    res.render('index',{
+      title:'首页',
+      keyword,
+      articles});
   });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(function(req,res,next){
  res.locals.user = req.session.user;
  res.locals.success = req.flash('success').toString();
  res.locals.error = req.flash('error').toString();
+ res.locals.keyword = '';
  next();
 });
 app.use(function(req,res,next){
